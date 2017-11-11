c846dff101f0ae20e3caae58a08f5fc8696c740b
Jonathan Wagner <jwagner95@gmail.com>
2017-11-11 08:48:55 -0500

    Add allocator support.

--- circular_buffer/src/cb_test.cpp
+++ circular_buffer/src/cb_test.cpp
@@ -5,6 +5,13 @@
 
 #include "circular_buffer.h"
 
+struct leak_checker {
+	static size_t count;
+	leak_checker() { ++count; }
+	~leak_checker() { --count; }
+};
+size_t leak_checker::count = 0;
+
 TEST_CASE("Constructor test" "[circular_buffer]")
 {
 	SECTION("Construct with capacity") {
@@ -15,15 +22,16 @@ TEST_CASE("Constructor test" "[circular_buffer]")
 		REQUIRE(cb.max_size() > 0);
 
 		cb.push_back(1);
-		REQUIRE(cb.front() == 1);
 		REQUIRE(cb.size() == 1);
 		REQUIRE(cb.capacity() == 5);
 		REQUIRE(!cb.empty());
+		REQUIRE(cb.front() == 1);
 
 		cb.push_back(2);
 		REQUIRE(cb.size() == 2);
 		REQUIRE(cb.capacity() == 5);
 		REQUIRE(!cb.empty());
+		REQUIRE(cb.front() == 1);
 
 		cb.push_back(3);
 		cb.push_back(4);
@@ -31,11 +39,61 @@ TEST_CASE("Constructor test" "[circular_buffer]")
 		REQUIRE(cb.size() == 5);
 		REQUIRE(cb.capacity() == 5);
 		REQUIRE(!cb.empty());
+		REQUIRE(cb.front() == 1);
 
 		cb.push_back(6);
 		REQUIRE(cb.size() == 5);
 		REQUIRE(cb.capacity() == 5);
 		REQUIRE(!cb.empty());
+		REQUIRE(cb.front() == 2);
 
+		cb.pop_front();
+		REQUIRE(cb.size() == 4);
+		REQUIRE(cb.capacity() == 5);
+		REQUIRE(!cb.empty());
+		REQUIRE(cb.front() == 3);
+
+		cb.pop_front();
+		REQUIRE(cb.size() == 3);
+		REQUIRE(cb.front() == 4);
+
+		cb.pop_front();
+		REQUIRE(cb.size() == 2);
+		REQUIRE(cb.front() == 5);
+
+		cb.pop_front();
+		REQUIRE(cb.size() == 1);
+		REQUIRE(cb.front() == 6);
+
+		cb.pop_front();
+		REQUIRE(cb.size() == 0);
+		REQUIRE(cb.capacity() == 5);
+		REQUIRE(cb.empty());
+
+		cb.push_back(7);
+		REQUIRE(cb.size() == 1);
+		REQUIRE(cb.capacity() == 5);
+		REQUIRE(!cb.empty());
+		REQUIRE(cb.front() == 7);
+
+		cb.push_back(8);
+		cb.push_back(9);
+		REQUIRE(cb.size() == 3);
+		REQUIRE(cb.capacity() == 5);
+		REQUIRE(!cb.empty());
+		REQUIRE(cb.front() == 7);
+
+		cb.clear();
+		REQUIRE(cb.size() == 0);
+		REQUIRE(cb.capacity() == 5);
+		REQUIRE(cb.empty());
+
+
+	}
+
+	SECTION("Check empty construction does not allocate members") {
+		REQUIRE(leak_checker::count == 0);
+		circular_buffer<leak_checker> cb(5);
+		REQUIRE(leak_checker::count == 0);
 	}
 }
--- circular_buffer/src/circular_buffer.h
+++ circular_buffer/src/circular_buffer.h
@@ -7,29 +7,43 @@
 #include <memory>
 #include <numeric>
 
-template <typename T>
+template <typename T, typename A = std::allocator<T>>
 class circular_buffer
 {
 public:
 	using value_type = T;
+	using allocator_type = A;
+	using self_type = circular_buffer<T, A>;
 	using size_type = std::size_t;
-	using difference_type = int;
-	using reference = value_type&;
-	using const_reference = const value_type&;
+	using difference_type = typename allocator_type::difference_type;
+	using reference = typename allocator_type::reference;
+	using const_reference = typename allocator_type::const_reference;
+	using pointer = typename allocator_type::pointer;
+	using const_pointer = typename allocator_type::const_pointer;
+
 	using class_type = circular_buffer;
 
-	explicit circular_buffer(std::size_t capacity)
+	explicit circular_buffer(std::size_t capacity, const allocator_type& allocator = allocator_type())
 		: m_capacity{ capacity },
-		m_buffer{ std::make_unique<value_type[]>(capacity) },
+		m_allocator{allocator},
+		m_buffer{ m_allocator.allocate(capacity)},
 		m_front{ nullptr },
-		m_back{ m_buffer.get() }
+		m_back{ m_buffer }
 	{}
+
+	~circular_buffer()
+	{
+		clear();
+		m_allocator.deallocate(m_buffer, m_capacity);
+	}
 
 	circular_buffer(const class_type&) = delete;
 	circular_buffer(class_type&&) = default;
 	class_type& operator=(const class_type &) = delete;
 	class_type& operator=(class_type &&) = default;
 	size_type capacity() const { return m_capacity; }
+
+	allocator_type get_allocator() const { return m_allocator; }
 
 	size_type size() const
 	{
@@ -39,9 +53,7 @@ public:
 
 	size_type max_size() const
 	{
-		const auto count = std::numeric_limits<size_type>::max() /
-			sizeof(value_type);
-		return std::max(count, size_type{ 1 });
+		return m_allocator.max_size();
 	}
 
 	bool empty() const
@@ -49,9 +61,16 @@ public:
 		return !m_front;
 	}
 
+	// This version of push_back will construct and destroy objects in m_buffer
+	// using the allocator's methods rather than traditional construction, copying
+	// and assignment.
 	bool push_back(const value_type &value)
 	{
-		*m_back = value; 
+		// If the buffer is full, old data will be deleted. 
+		if (m_front && m_front == m_back)
+			m_allocator.destroy(m_back);
+
+		m_allocator.construct(m_back, value);
 
 		value_type* const next = wrap(m_back + 1);
 		if (empty()) {
@@ -70,6 +89,30 @@ public:
 		}
 	}
 
+	void pop_front()
+	{
+		assert(m_front);
+
+		m_allocator.destroy(m_front);
+
+		value_type* const next = wrap(m_front + 1);
+		if (next == m_back)
+			m_front = nullptr;
+		else
+			m_front = next;
+	}
+
+	void clear()
+	{
+		if (m_front) {
+			do {
+				m_allocator.destroy(m_front);
+				m_front = wrap(m_front + 1);
+			} while (m_front != m_back);
+		}
+		m_front = nullptr;
+	}
+
 	reference front()
 	{
 		assert(m_front);
@@ -81,19 +124,20 @@ public:
 		assert(m_front);
 		return *m_front;
 	}
-	
+
 private:
 	value_type* wrap(value_type* ptr)
 	{
-		assert(ptr < m_buffer.get() + m_capacity * 2);
-		if (ptr >= m_buffer.get() + m_capacity)
+		assert(ptr < m_buffer + m_capacity * 2);
+		if (ptr >= m_buffer + m_capacity)
 			return ptr - m_capacity;
 		else
 			return ptr;
 	}
-
+	
 	const size_type m_capacity;
-	std::unique_ptr<value_type[]> m_buffer;
-	value_type* m_front;
-	value_type* m_back;
+	allocator_type m_allocator;
+	pointer m_buffer;
+	pointer m_front;
+	pointer m_back;
 };
